2a2cbbb2431594e36eb8728ee30c0351b251529c
jeronimoek <jeronimoek@gmail.com>
2023-03-14 13:47:23 -0300

    fix: update and add options

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ async function run() {
       packagePath,
       inputPath,
       outputPath,
+      options: { rootPaths: false },
     });
 
     const markdownChanged = markdownPrev !== markdownOutput;
